29b7040f0356ecefda61059a5fe6f5dfdca6c619
Styler <styler@streamelements.com>
2017-03-29 20:16:38 +0200

    Friday 5pm

--- main.js
+++ main.js
@@ -3,37 +3,47 @@ const {app, BrowserWindow, TouchBar, nativeImage, ipcMain} = require('electron')
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
+const _ = require('lodash');
 
 const {TouchBarLabel, TouchBarButton, TouchBarSpacer, TouchBarGroup} = TouchBar
-const gachiFolder = fs.readdirSync('gachi');
 
-const gachiIcon = nativeImage.createFromPath('./assets/billy.png');
+const menuItems = [];
+const audio = {};
+const icons = {
+  babyface: nativeImage.createFromPath('./assets/babyface.png'),
+  billy: nativeImage.createFromPath('./assets/BillyH.png'),
+  brother: nativeImage.createFromPath('./assets/Brother.png'),
+  Kazuya: nativeImage.createFromPath('./assets/Kazuya.png'),
+  markw: nativeImage.createFromPath('./assets/MarkW.png'),
+  ripped: nativeImage.createFromPath('./assets/Ripped.png'),
+  slave: nativeImage.createFromPath('./assets/Slave.png'),
+  unripped: nativeImage.createFromPath('./assets/Unripped.png'),
+  van: nativeImage.createFromPath('./assets/VanD.png')
+};
 
-const gachiButton = new TouchBarButton({
-  icon: gachiIcon,
-  click: () => {
-    const gachi = gachiFolder[Math.floor(Math.random() * gachiFolder.length)];
-    window.webContents.send('gachi', path.join(__dirname, 'gachi', gachi));
-  }
-});
-
-let window
-
-
-const menuItems = [gachiButton];
-
-gachiFolder.forEach(gachi => {
-  const item = new TouchBarButton({
-    label: gachi.replace('.mp3', ''),
-    // icon: gachiIcon,
+_.each(icons, (image, name) => {
+  fs.readdir(path.join(__dirname, 'assets', name), (err, sounds) => {
+    audio[name] = sounds;
+  });
+  const button = new TouchBarButton({
+    label: name,
+    backgroundColor: randomColor(),
+    icon: image.resize({
+      width: 40,
+      height: 40
+    }),
     click: () => {
-      window.webContents.send('gachi', path.join(__dirname, 'gachi', gachi));
-      console.log('Trying to play', gachi);
+      const gachi = _.sample(audio[button.label]);
+      window.webContents.send('gachi', path.join(__dirname, 'assets', button.label, gachi));
     }
   });
 
-  menuItems.push(item);
+  menuItems.push(button)
 });
+
+function randomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
 
 const touchBar = new TouchBar(menuItems)
 
